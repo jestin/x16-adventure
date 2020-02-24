a4ce2aa1b5138794fb98cf53725f149d2c1d79cb
Jestin Stoffel <jestin.stoffel@gmail.com>
2020-02-23 22:57:45 -0800

    Skeleton main loop functions

--- main.asm
+++ main.asm
@@ -25,27 +25,9 @@ mainloop:
 ;============================================================
 game_tick:
 
-	; hscroll layer 1
-	+vset (vreg_lay1 + 6) | AUTO_INC_1
-	lda zp_frame_counter_L4
-	sta veradat
-	lda zp_frame_counter_L4+1
-	sta veradat
-
-	; vscroll layer 1
-	+vset (vreg_lay1 + 8) | AUTO_INC_1
-	lda zp_frame_counter_L2
-	sta veradat
-	lda zp_frame_counter_L2+1
-	sta veradat
-
-	; hscroll layer 2
-	+vset (vreg_lay2 + 6) | AUTO_INC_1
-	lda zp_frame_counter
-	sta veradat
-	lda zp_frame_counter+1
-	sta veradat
-
+	jsr check_inputs
+	jsr update_game_state
+	jsr draw_screen
 	jsr update_frame_counters
 
 	rts
@@ -68,6 +50,47 @@ update_frame_counters:
 	+AslW zp_frame_counter_L2
 	+MoveW zp_frame_counter_L2, zp_frame_counter_L4
 	+AslW zp_frame_counter_L4
+	rts
+
+;============================================================
+; check_inputs
+; Get inputs from user
+;============================================================
+check_inputs:
+	rts
+
+;============================================================
+; update_game_state
+; Updates the state of the game
+;============================================================
+update_game_state:
+	; hscroll layer 1
+	+vset (vreg_lay1 + 6) | AUTO_INC_1
+	lda zp_frame_counter_L4
+	sta veradat
+	lda zp_frame_counter_L4+1
+	sta veradat
+
+	; vscroll layer 1
+	+vset (vreg_lay1 + 8) | AUTO_INC_1
+	lda zp_frame_counter_L2
+	sta veradat
+	lda zp_frame_counter_L2+1
+	sta veradat
+
+	; hscroll layer 2
+	+vset (vreg_lay2 + 6) | AUTO_INC_1
+	lda zp_frame_counter
+	sta veradat
+	lda zp_frame_counter+1
+	sta veradat
+	rts
+
+;============================================================
+; draw_screen
+; Draws the screen
+;============================================================
+draw_screen:
 	rts
 
 ;============================================================
